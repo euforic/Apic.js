100ea7efde6b42faa633e522b49feec8d2fc9b8d
euforic <cs@euforic.co>
2012-09-29 01:35:37 -0700

    fixed GET query issue

--- apic.js
+++ apic.js
@@ -54,14 +54,13 @@ if('undefined' !== typeof Titanium){
   Apic.prototype.add = function (namespace,methods){
     var self = this;
 
+    methods.forEach(function(method){
     // Create array if user passes single function
     if(!isArray(methods)){
       methods = [methods];
     }
 
     var ns = self.api[namespace] || (self.api[namespace] = {});
-
-    methods.forEach(function(method){
       //Create Method for Namespace
       ns[method.action] = function(data,cb){
 
@@ -79,7 +78,7 @@ if('undefined' !== typeof Titanium){
           .end(function(response){
             callback(response);
             // Debug Output
-            if(self.debug){ console.log('[HAPI DEBUG]:',{request:{ data: data, url: url }, response:response}); }
+            if(self.debug){ console.log('[HAPI DEBUG]:',{request:{ verb: verb, data: data, url: url }}); }
           });
       };
     });
